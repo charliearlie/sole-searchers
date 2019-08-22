7b8b9963f5a6693e91e964484a926f39e3ca50ff
Charlie Waite <cw5790@gmail.com>
2019-08-22 21:40:28 +0100

    Add new header with site logo and nav

---
+++ frontend/components/header/header.jsx
@@ -0,0 +1,62 @@
+import Link from 'next/link';
+import Router from 'next/router';
+import styled from 'styled-components';
+import NProgress from 'nprogress';
+
+import Subheader from './subheader';
+import Navigation from './navigation';
+
+Router.onRouteChangeStart = () => {
+  NProgress.start();
+};
+
+Router.onRouteChangeComplete = () => {
+  NProgress.done();
+};
+
+Router.onRouteChangeError = () => {
+  NProgress.done();
+};
+
+const Header = () => {
+  return (
+    <>
+      <StyledHeader>
+        <NavigationWrapper>
+          <div className="logo">Sole Searchers</div>
+          <Navigation />
+        </NavigationWrapper>
+        <Subheader />
+      </StyledHeader>
+    </>
+  );
+};
+
+export default Header;
+
+const StyledHeader = styled.header`
+  position: fixed;
+  background: white;
+  z-index: 3;
+  width: 100%;
+  /* box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.19); */
+  top: 0;
+  height: 106px;
+`;
+
+const NavigationWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  height: 60px;
+  justify-content: space-between;
+  max-width: ${props => props.theme.maxWidth};
+  margin: auto;
+
+  .logo {
+    color: ${props => props.theme.red};
+    font-family: 'Anton', 'Sans-serif';
+    font-size: 30px;
+    letter-spacing: 7px;
+    transform: skew(-5deg);
+  }
+`;
