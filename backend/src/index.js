7bc2c39e97d5e9fc868a9fc5ec358e24d6585e8c
Charlie Waite <cw5790@gmail.com>
2019-08-22 17:52:37 +0100

    Add server code to start the process

---
+++ backend/src/index.js
@@ -0,0 +1,47 @@
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+require("dotenv").config({ path: "variables.env" });
+const createServer = require("./createServer");
+const db = require("./db");
+
+const server = createServer();
+
+server.express.use(cookieParser());
+
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    req.userId = userId;
+  }
+  next();
+});
+
+server.express.use(async (req, res, next) => {
+  if (!req.userId) return next();
+
+  const user = await db.query.user(
+    {
+      where: {
+        id: req.userId
+      }
+    },
+    "{id, name, email, permissions}"
+  );
+
+  req.user = user;
+  next();
+});
+
+//Start
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    }
+  },
+  info => {
+    console.log(`server is running on http://localhost:${info.port}`);
+  }
+);
