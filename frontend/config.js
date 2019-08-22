b7bcc46b7e31171ef2c6aab65e64ae71e4c7b8dc
Charlie Waite <cw5790@gmail.com>
2019-08-22 16:16:56 +0100

    Add config file (nothing secret in here)

---
+++ frontend/config.js
@@ -0,0 +1,5 @@
+export const endpoint = `http://localhost:4444`;
+export const prodEndpoint = 'https://recipeze-yoga.herokuapp.com/';
+export const perPage = 8;
+export const recipesPerPage = 11;
+export const featuredRecipes = 5;
