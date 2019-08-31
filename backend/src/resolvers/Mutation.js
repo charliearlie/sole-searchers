5bc78d8a9f6f4ff5dd5c48627cddea6214968bfb
Charlie Waite <cw5790@gmail.com>
2019-08-31 12:10:23 +0100

    Add a current user query for the auth checking

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,9 +1,11 @@
 const { signIn, signOut, signUp } = require("./mutations/userMutations");
+const { createItem } = require("./mutations/itemMutations");
 
 const Mutations = {
   signIn,
   signOut,
-  signUp
+  signUp,
+  createItem
 };
 
 module.exports = Mutations;
