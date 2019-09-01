20f9c2976083ca296a48977c95f57f00ac64845c
Charlie Waite <cw5790@gmail.com>
2019-09-01 20:02:12 +0100

    Added Brand to item schema

--- backend/src/resolvers/mutations/itemMutations.js
+++ backend/src/resolvers/mutations/itemMutations.js
@@ -1,14 +1,7 @@
 async function createItem(parent, args, ctx, info) {
-  // if (!ctx.request.userId) {
-  //   throw new Error('You must be logged in to add an item to the database');
-  // }
   const data = {
     ...args
-    // images: { set: args.images },
-    // sizes: { set: args.sizes }
   };
-
-  console.log(data);
 
   const item = await ctx.db.mutation.createItem(
     {
