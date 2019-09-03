b20c6708bec7cd99e3cc91c65600dd3c3579d21b
Charlie Waite <cw5790@gmail.com>
2019-09-03 22:11:52 +0100

    Live exchange rates from EuropeanCentral Bank

--- frontend/context/currency-context.js
+++ frontend/context/currency-context.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { useCookies } from 'react-cookie';
-import { currencyReducer, CHANGE_CURRENCY } from './reducers';
+import { currencyReducer, CHANGE_CURRENCY, ADD_RATES } from './reducers';
 
 // This will probably become a user context but for now only currency is needed
 export const CurrencyContext = React.createContext();
 
-// Should probably move this into the context folder
 function CurrencyProvider(props) {
   const [cookies, setCookie] = useCookies([]);
   const userCurrency = cookies['currency'] || 'USD';
-  const [currency, dispatch] = React.useReducer(currencyReducer, userCurrency);
+  const [currency, dispatch] = React.useReducer(currencyReducer, {
+    currency: userCurrency,
+  });
+
+  React.useEffect(() => {
+    fetch('https://api.exchangeratesapi.io/latest?base=GBP')
+      .then(resp => resp.json())
+      .then(data => dispatch({ type: ADD_RATES, rates: data.rates }));
+  }, []);
 
   const changeCurrency = currency => {
     setCookie('currency', currency);
