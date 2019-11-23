cdc43bbcdcf821687511e94411b47675db71c9ca
Charlie Waite <cw5790@gmail.com>
2019-11-23 21:50:04 +0000

    Move query into item info as well as changing head

--- frontend/components/items/item-info.jsx
+++ frontend/components/items/item-info.jsx
@@ -1,29 +1,61 @@
-import styled from 'styled-components';
+import Head from 'next/head';
 import Link from 'next/link';
+import styled from 'styled-components';
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
 import Button from '../common/button';
 import ItemFinance from './item-finance';
 import ItemSizes from './item-sizes';
+import LoadingSpinner from '../loading-spinner';
 
-function ItemInfo({ item }) {
-  return (
-    <Wrapper>
-      <div className="image-container">
-        <img src={item.images} />
-      </div>
-      <div className="info">
-        <h3>{item.brand}</h3>
-        <h2>{item.title}</h2>
-        <ItemFinance price={item.price} />
-        <Link href="/deliveries-and-returns">
-          <DeliveryAndReturn>Delivery & return</DeliveryAndReturn>
-        </Link>
-        <ItemSizes />
-        <Button padding={15} borderRadius={0} buttonType="secondary">
-          ADD TO BASKET
-        </Button>
-      </div>
-    </Wrapper>
-  );
+export const ITEM_QUERY = gql`
+  query ITEM_QUERY($slug: String!) {
+    item(where: { slug: $slug }) {
+      brand
+      images
+      price
+      title
+    }
+  }
+`;
+
+function ItemInfo({ itemSlug }) {
+  const { data, error, loading } = useQuery(ITEM_QUERY, {
+    variables: { slug: itemSlug },
+  });
+
+  if (error) {
+    return <h2>Something went wrong</h2>;
+  }
+
+  if (loading) return <LoadingSpinner />;
+
+  if (data) {
+    const { item } = data;
+    return (
+      <Wrapper>
+        <Head>
+          <title>{item.title}</title>
+        </Head>
+        <div className="image-container">
+          <img src={item.images} />
+        </div>
+        <div className="info">
+          <h3>{item.brand}</h3>
+          <h2>{item.title}</h2>
+          <ItemFinance price={item.price} />
+          <Link href="/deliveries-and-returns">
+            <DeliveryAndReturn>Delivery & return</DeliveryAndReturn>
+          </Link>
+          <ItemSizes />
+          <Button padding={15} borderRadius={0} buttonType="secondary">
+            ADD TO BASKET
+          </Button>
+        </div>
+      </Wrapper>
+    );
+  }
+  return null;
 }
 
 const Wrapper = styled.div`
